7bd53ee60b652192dceb0adb4b87aa5b4bcae829
Nitya-Prakash <nityaprakashsahoo677@gmail.com>
2024-09-29 13:50:51 +0530

    Search-Suggestion-Feature

---
+++ script.js
@@ -0,0 +1,157 @@
+// Basically humain banana ye hai ki ek array hoga jismein objects honge aur har object main hoga kuch data. Datas are - ek image and ek name
+
+var arr = [
+  {
+    name: "Petals of roses",
+    image:
+      "https://images.unsplash.com/photo-1518621736915-f3b1c41bfd00?q=80&w=1386&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Animals of town",
+    image:
+      "https://images.unsplash.com/photo-1583337130417-3346a1be7dee?q=80&w=2688&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "The crowd of city",
+    image:
+      "https://images.unsplash.com/photo-1517732306149-e8f829eb588a?q=80&w=3872&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Orange peeled",
+    image:
+      "https://images.unsplash.com/photo-1557800636-894a64c1696f?q=80&w=3337&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Web design",
+    image:
+      "https://images.unsplash.com/photo-1581291518857-4e27b48ff24e?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Fruits of planet",
+    image:
+      "https://images.unsplash.com/photo-1576673442511-7e39b6545c87?q=80&w=3456&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Apple juice",
+    image:
+      "https://images.unsplash.com/photo-1626120032630-b51c96a544f5?q=80&w=1528&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "knowledge",
+    image:
+      "https://images.unsplash.com/photo-1457369804613-52c61a468e7d?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Product",
+    image:
+      "https://images.unsplash.com/photo-1518938630361-6245118b5415?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Autumn fruits",
+    image:
+      "https://images.unsplash.com/photo-1637652210657-e8361601c257?q=80&w=1370&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Pets",
+    image:
+      "https://images.unsplash.com/photo-1433162653888-a571db5ccccf?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Wanderlust",
+    image:
+      "https://images.unsplash.com/photo-1500530855697-b586d89ba3ee?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Lego",
+    image:
+      "https://images.unsplash.com/photo-1472457897821-70d3819a0e24?q=80&w=1469&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Colours",
+    image:
+      "https://images.unsplash.com/photo-1477414348463-c0eb7f1359b6?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Vintage",
+    image:
+      "https://images.unsplash.com/photo-1531525645387-7f14be1bdbbd?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Coloured Pins",
+    image:
+      "https://images.unsplash.com/photo-1597484662317-9bd7bdda2a45?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Yellow",
+    image:
+      "https://images.unsplash.com/photo-1485550409059-9afb054cada4?q=80&w=1530&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Door",
+    image:
+      "https://images.unsplash.com/photo-1504275107627-0c2ba7a43dba?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Pop art",
+    image:
+      "https://images.unsplash.com/photo-1649930536248-df58fd1f54f8?q=80&w=1530&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Exotic",
+    image:
+      "https://images.unsplash.com/photo-1680468196789-3a10379bf9ea?q=80&w=1366&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "London",
+    image:
+      "https://images.unsplash.com/photo-1592777336981-18334a076cef?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+];
+
+// Here 1st we created an array of objects containing sone images. To display all these images on the screen we have to create a function which will display all these images. 1st create a function named "showTheCards()" . Inside that function imply a forEach() loop an the array. Create a variable also inside that function nmaed "clutter" and leave it empty. Nowin the forEach() loop create a function and also pass a parameter "obj". Now inside that loop the local variable clutter will be store the HTML part of the image tag. In the image tag of the HTML part you have to put the object images in the src tag. Now select the "container" div form the HTML and save the clutter variable in that div. So the function will be iterate through all the elements of the array. When the function will run then the empty varibale will be created. And the function inside the loop will run. Corresponding images of the objects will save in the clutter and it will display.
+
+function showTheCards() {
+  var clutter = "";
+  arr.forEach(function (obj) {
+    clutter += `<div class="box">
+        <img class="cursor-pointer" src="${obj.image}" alt="image" loading="lazy">
+        <div class="caption text-black font-semibold">${obj.name}</div>
+    </div>`;
+  });
+
+  document.querySelector(".container").innerHTML = clutter;
+}
+
+function handleSearchFunctionality() {
+  var input = document.querySelector("#searchinput");
+  var overlay = document.querySelector(".overlay");
+  var searchdata = document.querySelector(".searchdata");
+
+  input.addEventListener("focus", function () {
+    overlay.style.display = "block";
+    searchdata.style.display = "block";
+  });
+
+  input.addEventListener("blur", function () {
+    overlay.style.display = "none";
+    searchdata.style.display = "none";
+  });
+
+  input.addEventListener("input", function () {
+    const filteredArray = arr.filter((obj) =>
+      obj.name.toLowerCase().startsWith(input.value)
+    );
+    var clutter = "";
+    filteredArray.forEach(function (obj) {
+      clutter += `<div class="res flex px-8 py-3">
+            <i class="ri-search-line font-semibold mr-5"></i>
+            <h3 class="font-semibold">${obj.name}</h3>
+        </div>`;
+    });
+
+    searchdata.innerHTML = clutter;
+  });
+}
+
+showTheCards();
+handleSearchFunctionality();
